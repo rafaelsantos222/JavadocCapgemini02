10e71d86579ecf66cc946aaa4cda9c547a35a3af
rafaelsantos222 <100045090+rafaelsantos222@users.noreply.github.com>
2022-02-20 13:28:03 -0300

    att codigo 20/02

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"questao02capgemini"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"questao02capgemini","l":"Questao02capgemini"}];updateSearchResults();
